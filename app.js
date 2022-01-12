4a81f1c5c42b4c401f208300b1f88e66a539621a
sylajen <33578533+sylajen@users.noreply.github.com>
2022-01-12 02:59:57 -0500

    2 Layered Avatar/Code Cleanup
    - 2 layered avatar
    - code cleanup

--- app.js
+++ app.js
@@ -24,3 +24,23 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+function hscrollbar() {
+    /* First, we need the horizontal scroll position of the viewer's display,
+       but there are different ways to call it in JS depending on browser.
+       I'm using the if/else shorthand notation to detect if a call is legit: 
+       somevar = (statement) ? statement_true_value : statement_false_value */
+    var left = 
+        /* window.pageXOffset should work for most recent browsers: */
+        window.pageXOffset ? window.pageXOffset : 
+        /* If it DOESN'T, let's try this: */
+        document.documentElement.scrollLeft ? document.documentElement.scrollLeft : 
+        /* And if THAT didn't work: */
+        document.body.scrollLeft;
+
+    /* Now that we have the horizontal scroll position, set #footpanel's left 
+       position to NEGATIVE the value, so it APPEARS to follow the scroll: */
+    document.getElementById('footpanel').style.left = -left;
+}
+window.onscroll = hscrollbar; /* Call the function when the user scrolls */
+window.onresize = hscrollbar; /* Call the function when the window resizes */
