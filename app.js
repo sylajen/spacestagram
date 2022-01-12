80cd3c4183606f637f90985e6fb9edc394159434
sylajen <33578533+sylajen@users.noreply.github.com>
2022-01-12 00:32:27 -0500

    Side Nav/Custom Avatar
    - added a side navigation bar with options to toggle dark mode and change avatar (not implemented yet, but will be there
    - added custom avatar in the top nav bar, will added various customizable options later

--- app.js
+++ app.js
@@ -14,3 +14,13 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
